e4336595426d77525db48c383385fee94a3ea1ef
fairbanksdp <fairbanksdp@s.dcsdk12.org>
2017-03-27 12:38:34 -0600

    Exercice 1

--- src/Card.java
+++ src/Card.java
@@ -33,6 +33,9 @@ public class Card {
 	 */
 	public Card(String cardRank, String cardSuit, int cardPointValue) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+    this.suit = cardSuit;
+    this.rank = cardRank;
+    this.pointValue = cardPointValue;
 	}
 
 
@@ -42,6 +45,7 @@ public class Card {
 	 */
 	public String suit() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+    return this.suit;
    }
 
 	/**
@@ -50,6 +54,7 @@ public class Card {
 	 */
 	public String rank() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+    return this.rank;
 	}
 
    /**
@@ -58,6 +63,7 @@ public class Card {
 	 */
 	public int pointValue() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+    return this.pointValue;
 	}
 
 	/** Compare this card with the argument.
@@ -68,6 +74,11 @@ public class Card {
 	 */
 	public boolean matches(Card otherCard) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+    if (otherCard.suit().equals(this.suit()) && 
+      otherCard.rank().equals(this.rank()) && 
+      otherCard.pointValue() == this.pointValue())  
+      return true;
+    return false;
 	}
 
 	/**
@@ -83,5 +94,6 @@ public class Card {
 	@Override
 	public String toString() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+    return this.rank()+" of "+this.suit()+" point value = "+this.pointValue();
 	}
 }
